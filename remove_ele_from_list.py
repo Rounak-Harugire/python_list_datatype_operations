3627fc672ee738c69b300658a94361dacec10f7d
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2024-09-20 22:06:53 +0530

    Add files via upload

---
+++ remove_ele_from_list.py
@@ -0,0 +1,8 @@
+# Creating a List
+List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Initial List:- ")
+print(List)
+List.remove(5)
+List.remove(6)
+print("\nafter Removal of two elements:- ")
+print(List)
