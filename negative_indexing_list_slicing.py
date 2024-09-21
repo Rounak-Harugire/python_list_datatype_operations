6b2be8e50935bcb710a493d071bec73261f0be28
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2024-09-21 22:20:06 +0530

    Add files via upload

---
+++ negative_indexing_list_slicing.py
@@ -0,0 +1,13 @@
+List =['R', 'O', 'U', 'N', 'A', 'K',
+        'H', 'A', 'R', 'U', 'G', 'I', 'R', 'E']
+print("Initial List: ")
+print(List)
+Sliced_List = List[:-6]
+print("\nElements sliced till 6th element from last: ")
+print(Sliced_List)
+Sliced_List = List[-6:-1]
+print("\nElements sliced from index -6 to -1")
+print(Sliced_List)
+Sliced_List = List[::-1]
+print("\nPrinting List in reverse: ")
+print(Sliced_List)
