e52a4210651ac6322fff3e453844ca02a6628ccc
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2024-09-21 22:16:28 +0530

    Add files via upload

---
+++ slicing_list.py
@@ -0,0 +1,14 @@
+List = ['R', 'O', 'U', 'N', 'A', 'K',
+        'H', 'A', 'R', 'U', 'G', 'I', 'R', 'E']
+print("Initial List: ")
+print(List)
+Sliced_List = List[3:8]
+print("\nSlicing elements in a range 3-8: ")
+print(Sliced_List)
+Sliced_List = List[5:]
+print("\nElements sliced from 5th "
+      "element till the end: ")
+print(Sliced_List)
+Sliced_List = List[:]
+print("\nPrinting all elements using slice operation: ")
+print(Sliced_List)
